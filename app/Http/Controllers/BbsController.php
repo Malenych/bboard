bca6bc7e340cdd84e4f2e86e4842106f243c74f6
Andrey Malenko <aamalenko@yandex.ru>
2021-10-15 15:39:52 +0300

    Страница детального просмотра объявления

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Bb;
 
 class BbsController extends Controller
 {
     public function index()
     {
-        return response('Здесь будет перечень объявлений.')
+        $bbs = Bb::latest()->get();
+        $s = "Объявления\r\n\r\n";
+        foreach ($bbs as $bb) {
+            $s .= $bb->title . "\r\n";
+            $s .= $bb->price . " руб.\r\n";
+            $s .= "\r\n";
+        }
+        return response($s)
             ->header('Content-Type', 'text/plain');
+    }
+
+    public function detail($bb)
+    {
+        $bb = Bb::find($bb);
+        $s = $bb->title . "\r\n\r\n";
+        $s .= $bb->content . "\r\n";
+        $s .= $bb->price . " руб.\r\n";
+        $s .= "\r\n";
+
+        return response($s)->header('Content-Type', 'text/plain');
     }
 }
