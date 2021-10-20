1e6e4a91f111d58eec2c7ee3cd28e498a6c102cf
Andrey Malenko <aamalenko@yandex.ru>
2021-10-20 19:45:33 +0300

    Добавлено разграничение доступа

---
+++ app/Policies/BbPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\Bb;
+
+class BbPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(User $user, Bb $bb)
+    {
+        return $bb->user->id === $user->id;
+    }
+
+    public function destroy(User $user, Bb $bb)
+    {
+        return $this->update($user, $bb);
+    }
+}
