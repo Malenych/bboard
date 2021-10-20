5afeea3b517020ed819d80dcbd8f27d90394ce74
Andrey Malenko <aamalenko@yandex.ru>
2021-10-20 18:55:27 +0300

    Добавлена валидация на страницы удаления и редактирования

--- resources/views/bb_add.blade.php
+++ resources/views/bb_add.blade.php
@@ -7,15 +7,30 @@
     @csrf
     <div class="form-group">
         <label for="txtTitle">Товар</label>
-        <input type="text" name="title" id="txtTitle" class="form-control">
+        <input type="text" name="title" id="txtTitle" class="form-control @error('title') is-invalid @enderror" value="{{ old('title') }}">
+        @error('title')
+        <span class="invalid-feedback">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
     </div>
     <div class="form-group">
         <label for="txtContent">Описание</label>
-        <textarea name="content" id="txtContent" class="form-control" row="3"></textarea>
+        <textarea name="content" id="txtContent" class="form-control @error('content') is-invalid @enderror" row="3">{{ old('content') }}</textarea>
+        @error('content')
+        <span class="invalid-feedback">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
     </div>
     <div class="form-group">
         <label for="txtPrice">Цена</label>
-        <input type="text" name="price" id="txtPrice" class="form-control">
+        <input type="text" name="price" id="txtPrice" class="form-control @error('price') is-invalid @enderror" value="{{ old('price') }}">
+        @error('price')
+        <span class="invalid-feedback">
+            <strong>{{ $message }}</strong>
+        </span>
+        @enderror
     </div>
     <input type="submit" class="btn btn-primary" value="Добавить">
 </form>
